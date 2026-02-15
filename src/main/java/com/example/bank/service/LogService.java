0dde32e0a68295d0d6dab13b554cef6dd38e81ad
meatcarrot <meatcarrot@gmail.com>
2026-02-15 14:21:49 +0900

    fit: Transactional Event Listener Test

--- src/main/java/com/example/bank/service/LogService.java
+++ src/main/java/com/example/bank/service/LogService.java
@@ -11,6 +11,8 @@ import static org.springframework.transaction.annotation.Propagation.REQUIRES_NE
 public class LogService {
 
     @Transactional(propagation = REQUIRES_NEW)
+    // 호출한 쪽이 트랜잭션이 있든 없든, 무조건 나만의 길을 가겠다
+    // 안전장치도 겸함
     public void logWithdraw() {
         System.out.println("===== LogService.logWithdraw =====");
         System.out.println("트랜잭션 활성화 여부: "
